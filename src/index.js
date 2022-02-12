442e881e9b706065e5dc5551353c8379c156ecca
mattmuroya <matt.muroya@gmail.com>
2022-02-12 00:42:23 -1000

    add shade behind to mobile menu

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import favicon from './img/favicon.ico'
 
 const menuBtn = document.getElementById('menu-btn');
 const sidebar = document.getElementById('sidebar');
+const closeMenuBtn = document.getElementById('close-menu-btn');
+const shade = document.getElementById('shade');
 
 menuBtn.addEventListener('click', () => {
   sidebar.classList.toggle('-translate-x-full');
+  shade.classList.toggle('hidden');
 });
 
-const closeMenuBtn = document.getElementById('close-menu-btn');
 closeMenuBtn.addEventListener('click', () => {
   sidebar.classList.toggle('-translate-x-full');
+  shade.classList.toggle('hidden');
 });
+
+shade.addEventListener('click', () => {
+  sidebar.classList.toggle('-translate-x-full');
+  shade.classList.toggle('hidden');
+});
+                                                         
