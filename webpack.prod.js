abd94ff5d51c21a9d5c67570844d6c8099751509
mattmuroya <matt.muroya@gmail.com>
2022-02-15 17:51:51 -1000

    add contenthash to filename output for cache busting

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'main-[contenthash].js',
     assetModuleFilename: './[name][ext]',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
@@ -20,7 +20,7 @@ module.exports = {
       template: './src/template.html',
     }),
     new MiniCssExtractPlugin({
-      filename: 'main.css',
+      filename: 'main-[contenthash].css',
     }),
   ],
   module: {
